20550716f7ff4f4fe8cc99e7607325a5e320c2f1
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2025-04-21 20:46:03 +0300

    Update ModalForm.jsx

--- frontend/src/components/ModalForm.jsx
+++ frontend/src/components/ModalForm.jsx
@@ -23,15 +23,36 @@ export default function ModalForm({ isOpen, onClose, mode, onSubmit }) {
             >
               ✕
             </button>
+            <label className="input input-bordered flex items-center gap-2 w-full my-2">
+              <span className="label-text">Name</span>
+              <input type="text" placeholder="Type here" className="grow" />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 w-full my-2">
+              <span className="label-text">Email</span>
+              <input type="text" placeholder="Type here" className="grow" />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 w-full my-2">
+              <span className="label-text">Job</span>
+              <input type="text" placeholder="Type here" className="grow" />
+            </label>
+            <div className="flex mb-2 justify-between gap-2 w-full">
+              <label className="input input-bordered flex items-center gap-2 w-1/2">
+                <span className="label-text">Rate</span>
+                <input type="number" placeholder="Type here" className="grow" />
+              </label>
+              <select className="select select-bordered w-1/2">
+                <option>Active</option>
+                <option>Inactive</option>
+              </select>
+            </div>
             <button
-              className="btn btn-success btn-sm"
+              className="btn btn-success btn-sm float-end"
               onClick={onSubmit}
               type="submit"
             >
               {mode === "add" ? "Add Client" : "Save Changes"}
             </button>
           </form>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
         </div>
       </dialog>
     </>
